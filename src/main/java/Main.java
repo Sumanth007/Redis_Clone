60f1bcc925fc3d0b281f1cee8728ede269a54653
root <root@Sumanth>
2023-08-15 17:16:57 +0530

    Responding for PING

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,10 +17,30 @@ public class Main {
        int port = 6379;
        try {
          serverSocket = new ServerSocket(port);
-         serverSocket.setReuseAddress(true);
-         // Wait for connection from client.
-         clientSocket = serverSocket.accept();
-       } catch (IOException e) {
+        //  serverSocket.setReuseAddress(true);
+        //  // Wait for connection from client.
+        //  clientSocket = serverSocket.accept();
+        // serverSocket.setSoTimeout(1000); 
+        System.out.println("Heeeyyy");
+      while(true) {
+			System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "..."); 
+
+			Socket server = serverSocket.accept();
+			System.out.println("Just connected to " + server.getRemoteSocketAddress()); 
+
+			PrintWriter toClient = 
+				new PrintWriter(server.getOutputStream(),true);
+			BufferedReader fromClient =
+				new BufferedReader(
+						new InputStreamReader(server.getInputStream()));
+			String line = fromClient.readLine();
+			System.out.println("Server received: " + line); 
+			toClient.println("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");         
+      server.close();
+
+       } 
+      }
+       catch (IOException e) {
          System.out.println("IOException: " + e.getMessage());
        } finally {
          try {
@@ -27,5 +51,7 @@ public class Main {
            System.out.println("IOException: " + e.getMessage());
          }
        }
+
+
   }
 }
