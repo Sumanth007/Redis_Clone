8d6383d40bb0a2a60348b53e8fbe6b53d3b3683a
root <root@Sumanth>
2023-08-15 17:19:35 +0530

    PING - PONG 0.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,6 +34,7 @@ public class Main {
 				new BufferedReader(
 						new InputStreamReader(server.getInputStream()));
 			String line = fromClient.readLine();
+      System.out.println("PONG");
 			System.out.println("Server received: " + line); 
 			toClient.println("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");         
       server.close();
