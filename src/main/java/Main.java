584d98e0fc36082a407192673a401f6e3a644337
root <root@Sumanth>
2023-08-15 19:14:27 +0530

    Multiple Pong

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args){
@@ -28,14 +33,25 @@ public class Main {
 			clientSocket = serverSocket.accept();
 			System.out.println("Just connected to " + clientSocket.getRemoteSocketAddress()); 
 
-			PrintWriter toClient = 
-				new PrintWriter(clientSocket.getOutputStream(),true);
+			BufferedWriter toClient = 
+				new BufferedWriter(
+          new OutputStreamWriter(clientSocket.getOutputStream()));
+
 			BufferedReader fromClient =
 				new BufferedReader(
 						new InputStreamReader(clientSocket.getInputStream()));
-			String line = fromClient.readLine();
+
+      while(true){
+      String line = fromClient.readLine();
 			System.out.println("Server received: " + line); 
-      toClient.println("+PONG\r");
+      toClient.write("+PONG\r\n");
+      toClient.flush();
+      toClient.write("Press \" close \" to close the connection");
+      toClient.flush();
+      String check= fromClient.readLine();
+      if(check.equals("close"))break;
+    }
+      
 			// toClient.println("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");         
       toClient.close();
 
